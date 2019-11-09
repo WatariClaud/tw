1e57c1dde7bc37e09b94609f871acca65e998abe
Claud Watari <claudwatari95@gmail.com>
2019-11-09 07:44:24 +0300

    fix-userRelation

--- specs/server.test.js
+++ specs/server.test.js
@@ -1,5 +1,7 @@
 import chai from 'chai';
 
+import pg from 'pg';
+
 import chaiHttp from 'chai-http';
 
 import server from '../source/server';
@@ -10,7 +12,37 @@ const expect = chai.expect;
 
 chai.use(chaiHttp);
 
+const Pool = pg.Pool;
+
+const pool = new Pool({
+  user: config.dbUser,
+  host: config.DbH,
+  database: config.dbName,
+  password: config.dbPass,
+  port: config.dbPort,
+});
+
 describe('Create user', () => {
+
+  before('add table', (done) => {
+    const query = `CREATE TABLE users (id SERIAL PRIMARY KEY,
+    firstName VARCHAR(30), lastName VARCHAR(30), email VARCHAR(50),
+    password VARCHAR(255), gender VARCHAR(11), jobRole VARCHAR(50),
+    department VARCHAR(50), address VARCHAR(30)`;
+
+    pool.query(query, (err, res));
+
+    done();
+  });
+
+  after('clear table', (done) => {
+    const query = `DROP TABLE users`;
+
+    pool.query(query, (err, res));
+
+    done();
+  })
+
   it('should not sign user up if email exists', (done) => {
     chai.request(server)
       .post('/api/v1/auth/create-user')
