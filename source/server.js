3e9c10dc4b5688123b103aae537d81c2f128ed56
Claud Watari <claudwatari95@gmail.com>
2019-11-22 05:59:38 +0300

    bug-fixCors

--- source/server.js
+++ source/server.js
@@ -1,8 +1,20 @@
 import express from 'express';
+
 import router from '../routes/routes';
+
 import bodyParser from 'body-parser';
 
+import cors from 'cors';
+
 const app = express();
+
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  next();
+})
 app.use(express.json());
 
 app.use(bodyParser.json())
